cf3d2c8aa34c47f3b8e0eb44a9f9521dca9fe68d
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-09-14 15:54:10 +0300

    Create Next.js, initialize backend & connect to MongoDB

---
+++ config/dbConnect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const dbConnect = () => {
+    if(mongoose.connection.readyState >= 1) {
+        return
+    }
+
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        UseUnifiedTopology: true,
+    }).then(con => console.log('Connected to local database'))
+}
+
+export default dbConnect;
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    DB_LOCAL_URI: "mongodb://0.0.0.0:27017/"
+  }
+}
