916a5fc3d52c703182074a7f28ae729407366bb6
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-09-19 08:41:19 +0300

    Create JSON file & create a data seeder

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -8,16 +8,19 @@ const allRooms = async(req, res) => {
 
     res.status(200).json({
         success: true,
+        count: rooms.length,
         rooms
     })
         
     } catch (error) {
-        req.status(400).json({
-            success : false,
-            error : error.message
+        res.status(400).json({
+            success: false,
+            error: error.message
         })
         
     }
+
+    
 }
 
 //Create new rooms
@@ -25,22 +28,23 @@ const allRooms = async(req, res) => {
 const newRoom = async (req, res) => {
 
     try {
-
-    const rooms = await Room.create(req.body);
+    
+    const room = await Room.create(req.body);
 
     res.status(200).json({
-        success : true,
-        count : rooms.length,
-        rooms
+        success: true,
+        room
     })
         
     } catch (error) {
         res.status(400).json({
-            success : false,
-            error : error.message
-        })   
+            success: false,
+            error: error.messege
+        })
+        
     }
 
 }
+
 
 export {allRooms, newRoom}
