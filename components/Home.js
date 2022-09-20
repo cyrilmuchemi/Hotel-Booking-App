c722c90102dcf4f1f4b71826693e97c2f40635a3
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-09-20 15:35:02 +0300

    Initialize redux & configure store

---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    <h1>Fuck me in the diabz!</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ redux/store.js
@@ -0,0 +1,33 @@
+import { configureStore, applyMiddleware } from 'redux';
+import { HYDRATE, createWrapper } from 'next-redux-wrapper';
+import thunkMiddleware from 'redux-thunk';
+import reducers from './reducers/reducers';
+
+
+const bindMiddlware = (middleware) => {
+    if(process.env.NODE_ENV !== 'production') {
+        const { composeWithDevTools } = require('redux-devtools-extension')
+        return composeWithDevTools(applyMiddleware(...middleware))
+    }
+
+    return applyMiddleware(...middleware)
+}
+
+const reducer = (state, action) => {
+    if(action.type == HYDRATE) {
+        const nextState = {
+            ...state,
+            ...action.payload
+        }
+
+        return nextState
+    } else {
+        return reducers(state, action)
+    }
+}
+
+const initStore = () => {
+    return configureStore(reducer, bindMiddlware([thunkMiddleware]))
+}
+
+export const wrapper = createWrapper(initStore)
