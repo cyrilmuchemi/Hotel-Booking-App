ca3301bb189b613b2118f64a1eed5ddb3946a012
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-09-23 09:22:31 +0300

    Add search components that filters location

--- components/Home.js
+++ components/Home.js
@@ -1,28 +1,50 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import RoomItem from './room/RoomItem';
+import Pagination from "react-js-pagination";
 import { toast } from 'react-toastify';
 import { clearErros } from '../redux/actions/roomActions';
+import { useRouter } from 'next/router'; 
+import Link from 'next/Link';
 
 const Home = () => {
 
   const dispatch = useDispatch();
 
-  const { rooms, error } = useSelector(state=>state.allRooms);
+  const router = useRouter();
+
+  const { rooms, resPerPage, roomsCount, filteredRoomsCount ,error } = useSelector(state=>state.allRooms);
+
+  let { location, page = 1 } = router.query;
+  page = Number(page)
+
 
   useEffect(() => {
     toast.error(error)
     dispatch(clearErros())
   }, [])
 
-  return (
-    <section id='rooms' className='container mt-5'>
-    <h2 className='mb-3 ml-2 stays-heading'>Stays in New York</h2>
+  const hanglePagination = (pageNumber) => {
+    window.location.href = `/?page=${pageNumber}`
 
-    <a href='#' className='ml-2 back-to-search'>
-      {' '}
+  }
+
+  let count = roomsCount;
+
+  if(location) {
+    count = filteredRoomsCount
+  }
+
+  return (
+    <>
+    <section id='rooms' className='container mt-5'>
+    <h2 className='mb-3 ml-2 stays-heading'>{location ? `Rooms in ${location}`: 'All Rooms'}</h2>
+
+    <Link href='/search'>
+      <a  className='ml-2 back-to-search'>
       <i className='fa fa-arrow-left'></i> Back to Search
-    </a>
+      </a>
+    </Link>
     <div className='row'>
       {rooms && rooms.length == 0 ? 
         <div className='alert alert-danger'><b>No Rooms.</b></div>
@@ -33,6 +55,23 @@ const Home = () => {
       }
     </div>
   </section>
+  {resPerPage < count &&
+  <div className='d-flex justify-content-center mt-5'>
+  <Pagination
+    activePage={page}
+    itemsCountPerPage={resPerPage}
+    totalItemsCount={roomsCount}
+    onChange={hanglePagination}
+    nextPageText={'Next'}
+    prevPageText={'Prev'}
+    firstPageText={'First'}
+    lastPageText={'Last'}
+    itemClass='page-item'
+    linkClass='page-link'
+  />
+  </div>
+  }
+  </>
   )
 }
 
--- redux/actions/roomActions.js
+++ redux/actions/roomActions.js
@@ -10,12 +10,12 @@ import {
 
 //Get all rooms
 
-export const getRooms = (req) => async (dispatch) => {
+export const getRooms = (req, currentPage = 1, location='') => async (dispatch) => {
     try {
 
     const { origin } = absoluteUrl(req)
 
-    const { data } = await axios.get(`${origin}/api/rooms`)
+    const { data } = await axios.get(`${origin}/api/rooms?page=${currentPage}&location=${location}`)
 
 
        dispatch({
