0259b78e04dccaede8c88e29fa343c6bfe944765
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-09-28 10:08:55 +0300

    Protect user routes

---
+++ pages/me/update.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { getSession } from 'next-auth/react';
+
+const updateProfilePage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+
+  const session = await getSession({req: context.req})
+
+  if(!session) {
+    return {
+        redirect: {
+            destination: '/login',
+            permanent: false
+        }
+
+    }
+  }
+
+  return {
+    props: { session }
+  }
+    
+}
+
+export default updateProfilePage;
