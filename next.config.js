7fa08f43a23f619a7d5708f09b061385bf43c2ea
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-10-04 13:35:25 +0300

    Setup Stripe in server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   env: {
     DB_LOCAL_URI: "mongodb://0.0.0.0:27017/",
+
+    STRIPE_API_KEY: 'pk_test_51Lp5WEEMnCAQAhVdgsdP9CLGHS5qkN1Q4lOQP4Jh2kzZqrh2pgtoK6Z71s1MO38k2quXkzJsToZiOoS46jyTUw5000cJ3tt4Dp',
+
+    STRIPE_SECRET_KEY: 'sk_test_51Lp5WEEMnCAQAhVdoiQfE31dlzycg7RehOFYUg8wu2EK0GYiqjbqsThk8wyt2TWwcmI1JcNi9KNn3f1juLI99tBl00ChjGdQbE',
+
     CLOUDINARY_CLOUD_NAME: 'dyemjook1',
     CLOUDINARY_API_KEY: '171949296453622',
     CLOUDINARY_API_SECRET: 'bL6XbPZDaEKZ96xCY4l8Ygjqajs',
