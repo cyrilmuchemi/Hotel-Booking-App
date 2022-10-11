5ab55f871303d3303363f38db28d8669ccf41741
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-10-11 12:01:41 +0300

    Fix more deployment errors

--- components/user/Profile.js
+++ components/user/Profile.js
@@ -46,7 +46,7 @@ const Profile = () => {
             dispatch({ type: UPDATE_PROFILE_RESET })
         }
 
-    }, [dispatch, isUpdated, error, loadedUser])
+    }, [dispatch, isUpdated, error, loadedUser, router])
 
 
     const submitHandler = (e) => {
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -7,9 +7,6 @@ const dbConnect = () => {
 
     mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
     })
 
 }
