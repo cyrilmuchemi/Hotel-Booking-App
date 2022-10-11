d08e8f5d5e305000fd8bf81b75e0c57bd2c197a9
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-10-11 11:38:25 +0300

    Make some tweaks

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -8,8 +8,6 @@ const dbConnect = () => {
     mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
     })
 
 }
